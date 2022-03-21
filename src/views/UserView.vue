e671c56f70963743bfd80c5cd1728564e5291181
zhangtuo <13120409722@163.com>
2022-03-21 09:35:51 +0800

    添加 UserView；使用动路由参数

---
+++ src/views/UserView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1> userID： {{ currentID }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserView",
+  data() {
+    return {
+      currentID: undefined,
+    }
+  },
+  created() {
+    this.currentID = this.$route.params.id
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
